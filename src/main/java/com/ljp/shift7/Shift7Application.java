7ce0f375f796a02651805f4170779eea9801ae56
13617 <136174298@qq.com>
2023-09-09 16:48:55 +0900

    内容沾到剪贴板

--- src/main/java/com/ljp/shift7/Shift7Application.java
+++ src/main/java/com/ljp/shift7/Shift7Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
 import java.util.List;
 import java.util.Map;
 
@@ -15,7 +19,12 @@ public class Shift7Application {
 
 		List<Map<String, String>> def = Main.readFromExlDef();
 		Map<String, String> names =Main. readFromExlName();
-		System.out.println(Main.outPut(Main.readFromExl(def,names)));
+		String ans = Main.outPut(Main.readFromExl(def, names));
+		System.out.println(ans);
+
+		Clipboard systemClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		Transferable tText = new StringSelection(ans);
+		systemClipboard.setContents(tText,null);
 	}
 
 }
